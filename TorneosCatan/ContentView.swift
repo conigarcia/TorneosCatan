a9ac7ccfe1f6a3f898cc00cb86cb84ec7e2c16bd
Constanza Garcia <conigarciadiaz@gmail.com>
2024-05-18 13:08:46 -0300

    Initial Commit

---
+++ TorneosCatan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TorneosCatan
+//
+//  Created by coni garcia on 18/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TorneosCatan/TorneosCatanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TorneosCatanApp.swift
+//  TorneosCatan
+//
+//  Created by coni garcia on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TorneosCatanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
